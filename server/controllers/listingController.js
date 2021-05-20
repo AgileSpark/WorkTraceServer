29e7c329c02a569c5c7ff90ce168a5e84fbd412f
MadinventorZero <42157117+MadinventorZero@users.noreply.github.com>
2021-05-20 15:10:37 -0700

    Added an initial status of 0 for new listings
    Co-authored-by: buddhajjigae <buddhajjigae@users.noreply.github.com>
    Co-authored-by: peterhyesung <peterhyesung@users.noreply.github.com>
    Co-authored-by: dieunity <dieunity@users.noreply.github.com>

--- server/controllers/listingController.js
+++ server/controllers/listingController.js
@@ -24,6 +24,29 @@ listingController.getListings = (req, res, next) => {
 
 listingController.addListing = (req, res, next) => {
   const {user_id, company, source, location, url} = req.body
+  const status = 0;
+  console.log('Inputs', user_id, company, source, location, url)
+  const newListingQuery = `
+  INSERT INTO Listings (user_id, company, source, location, url, status)
+  VALUES ($1, $2, $3, $4, $5, $6)
+  RETURNING *
+  `
+  const values = [user_id, company, source, location, url, status];
+  try {
+    db.query(newListingQuery, values, (err, result) => {
+      if (result) {
+        const {rows} = result;
+        console.log('Row data', rows[0])
+        res.locals.addedListing = rows[0];
+        return next();
+      }
+      else return next();
+    });
+  } catch (err) {next(err);}
+};
+
+listingController.updateStatus = (req, res, next) => {
+  const {listing_id, status} = req.body
   console.log('Inputs', user_id, company, source, location, url)
   const newListingQuery = `
   INSERT INTO Listings (user_id, company, source, location, url)
