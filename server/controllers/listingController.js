76e13c7690000a96d29841d6e2292154d2d34f01
MadinventorZero <42157117+MadinventorZero@users.noreply.github.com>
2021-05-20 14:15:28 -0700

    Semi-complete addListing & getListing routes
    Co-authored-by: buddhajjigae <buddhajjigae@users.noreply.github.com>
    Co-authored-by: peterhyesung <peterhyesung@users.noreply.github.com>
    Co-authored-by: dieunity <dieunity@users.noreply.github.com>

--- server/controllers/listingController.js
+++ server/controllers/listingController.js
@@ -3,43 +3,43 @@ const db = require ('../schemas/schemaSQL');
 const listingController = {};
 
 listingController.getListings = (req, res, next) => {
-  const listingQuery = 
-    `SELECT ***
-    FROM ****
-    `;
+  const {user_id} = req.body
+  
+  const getListingsQuery = `
+  SELECT *
+  FROM Listings
+  WHERE user_id='${user_id}'
+  `
   try {
-    db.query(listingQuery, (err, result) => {
-      const {rows} = result;
-      res.locals.userListings = rows;
-      return next();
-    });
-  } catch (err) {next(err);}
-};
-
-listingController.getListingDetail = (req, res, next) => {
-  const listingQuery = 
-    `SELECT ***
-    FROM ****
-    `;
-  try {
-    db.query(listingQuery, (err, result) => {
-      const {rows} = result;
-      res.locals.userListings = rows;
-      return next();
+    db.query(getListingsQuery, (err, result) => {
+      if (result) {
+        const {rows} = result;
+        res.locals.userListings = rows;
+        return next();
+      }
+      else return next();
     });
   } catch (err) {next(err);}
 };
 
 listingController.addListing = (req, res, next) => {
-  const listingQuery = 
-    `SELECT ***
-    FROM ****
-    `;
+  const {user_id, company, source, location, url} = req.body
+  console.log('Inputs', user_id, company, source, location, url)
+  const newListingQuery = `
+  INSERT INTO Listings (user_id, company, source, location, url)
+  VALUES ($1, $2, $3, $4, $5)
+  RETURNING *
+  `
+  const values = [user_id, company, source, location, url];
   try {
-    db.query(listingQuery, (err, result) => {
-      const {rows} = result;
-      res.locals.userListings = rows;
-      return next();
+    db.query(newListingQuery, values, (err, result) => {
+      if (result) {
+        const {rows} = result;
+        console.log('Row data', rows[0])
+        res.locals.addedListing = rows[0];
+        return next();
+      }
+      else return next();
     });
   } catch (err) {next(err);}
 };
--- server/routes/listingRouter.js
+++ server/routes/listingRouter.js
@@ -4,13 +4,24 @@ const sessionController = require('../controllers/sessionController');
 const listingRouter = express.Router();
 
 // set cookie -> create user -> start session -> set SSID cookie
-// listingRouter.post('/addListing', sessionController.loggedIn, listingController.addListing, (req, res) => {
-
-// })
+listingRouter.post('/addListing', listingController.addListing, (req, res) => {
+  console.log('Made it to the end of add/listing')
+  if (res.locals.addedListing) {
+    res.status(200).json({addedJob: res.locals.addedListing})
+  } else {
+    res.status(200).json({jobNotAdded: true})
+  }
+})
 
 // set cookie -> check if logged In -> verify user (if not loggedin) -> start session (or renew session) - set (or renew) SSID cookie
-// listingRouter.get('/getListings', sessionController.loggedIn, listingController.getListings, (req, res) => {
-  
-// })
+listingRouter.get('/getListings', listingController.getListings, (req, res) => {
+  console.log('Made it to the get listing route')
+  if (res.locals.userListings) {
+    res.status(200).json({userListings: res.locals.userListings})
+  } else {
+    res.status(200).json({jobsNotFound: true})
+  }
+
+})
 
 module.exports = listingRouter;
