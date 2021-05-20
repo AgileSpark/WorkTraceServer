b8c6b2ebe04a097fc9858beca7c6f21139f3844c
MadinventorZero <42157117+MadinventorZero@users.noreply.github.com>
2021-05-19 19:42:46 -0700

    Additional Files for server structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,11 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     devServer: {
         port: '8080',
         hot: true,
         compress: true,
-        contentBase: path.resolve(__dirname, './src'),
+        contentBase: path.resolve(__dirname, './index.html'),
         watchContentBase: true,
         historyApiFallback: true,
         proxy: {
@@ -24,14 +21,11 @@ module.exports = {
         }
     },
     entry: {
-        popup: path.resolve(__dirname, "./src/index-popup.js"),
-        options: path.resolve(__dirname, "./src/index-options.js"),
-        foreground: path.resolve(__dirname, "./src/index-foreground.js")
+        server: path.resolve(__dirname, "./server/server.js")
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        // publicPath: '/dist'
     },
     module: {
         rules: [
@@ -58,30 +52,4 @@ module.exports = {
             }
         ]
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'popup.html',
-            template: 'src/popup.html',
-            chunks: ['popup']
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'options.html',
-            template: 'src/options.html',
-            chunks: ['options']
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'foreground.html',
-            template: 'src/foreground.html',
-            chunks: ['foreground']
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'src/manifest.json', to: '[name].[ext]' },
-                { from: 'src/background.js', to: '[name].[ext]' },
-                { from: 'src/inject_script.js', to: '[name].[ext]' },
-                { from: 'src/assets/*.png', to: '[name].[ext]' }
-            ]
-        }),
-        new CleanWebpackPlugin()
-    ]
 }
